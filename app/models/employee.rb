997e750247c3753d1b592aedbcd4bebdfb5e3ed9
Sandra Alvarez <sandra.alvarez.ramos18@gmail.com>
2025-02-03 18:06:36 -0600

    Valores en default

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,7 +4,7 @@ class Employee < ApplicationRecord
   belongs_to :company
   # has_one_attached :avatar
 
-  validates :name, presence: true, length: { minimum: 3 }
+  validates :name, presence: true
   validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 18 }
   validates :id_number, presence: true, uniqueness: true
 end
--- db/migrate/20250203045548_create_employees.rb
+++ db/migrate/20250203045548_create_employees.rb
@@ -1,9 +1,10 @@
 class CreateEmployees < ActiveRecord::Migration[7.1]
   def change
     create_table :employees do |t|
-      t.string :name
-      t.integer :age
-      t.string :id_number
+      t.string :name, null: false, default: "Empleado Nuevo" #
+      t.integer :age,  null: false, default: 0
+      t.string :id_number, null: false, unique: true #
+      
       t.references :role, null: false, foreign_key: true
       t.references :department, null: false, foreign_key: true
       t.references :company, null: false, foreign_key: true
