83e0e1df61ca40c4c51b6ce507901ac3f1e06cc3
Sandra Alvarez <sandra.alvarez.ramos18@gmail.com>
2025-02-05 14:49:45 -0600

    Controlador employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,39 +1,59 @@
 class EmployeesController < ApplicationController
 
+  before_action :set_company
   before_action :set_employee, only: [:show, :edit, :update, :destroy]
 
   def index
-    @employees = Employee.company
-
-  end
-
-  def new
-  end
-
-  def create
+    @employees = @company.employees
   end
 
   def show
+  end
+
+  def new
+    @employee = @company.employees.build
+  end
+
+  def create
+    @employee = @company.employees.build(employee_params)
+    if @employee.save
+      redirect_to company_employees_path(@company), notice: "Empleado creado con éxito."
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @employee.update(employee_params)
+      redirect_to company_employee_path(@company, @employee), notice: "Empleado actualizado con éxito."
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @employee.destroy
+    redirect_to company_employees_path(@company), notice: "Empleado eliminado con éxito."
   end
 
   private
 
-  def set_employee
+  def set_company
+    @company = current_user.companies.find_by(id: params[:company_id])
+    redirect_to root_path, alert: "Compañía no encontrada" if @company.nil?
   end
+
+
+  def set_employee
+    @employee = @company.employees.find_by(id: params[:id])
+    redirect_to company_employees_path(@company), alert: "Empleado no encontrado" if @employee.nil?
+  end
+
 
   def employee_params
     params.require(:employee).permit(:name, :birth_date, :id_number, :role_id, :department_id, :avatar)
   end
-end
-
-
 end
